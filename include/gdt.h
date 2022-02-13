d4bf0d189a26eab341fed47b6d6a8f532868c881
w112785@gmail.com <w112785@gmail.com>
2022-02-13 11:12:49 -0500

    moved all code from Master Branch to Live Branch

--- include/gdt.h
+++
@@ -1,6 +0,0 @@
-#ifndef gdt_h
-#define gdt_h
-
-extern void init_gdt();
-
-#endif
--- include/idt.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef idt_h
-#define idt_h
-
-void idt_install();
-void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
-
-
-#endif //idt_h
